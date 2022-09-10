cd69628184d357b6f8065cac4d47505dab83010d
Vladlen Damilin <danilinwladyslaw@gmail.com>
2022-09-10 22:43:25 +0400

    add mvvm architecture pattern

---
+++ AniSpace/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AniSpace.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
---
+++ AniSpace/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AniSpace.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniSpace.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+    }
+}

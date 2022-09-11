4ba2034c41ae20dd40efe9eebddef0539ea4e861
Vladlen Damilin <danilinwladyslaw@gmail.com>
2022-09-11 08:46:10 +0400

    add AsyncCommand

---
+++ AniSpace/Infructuctre/Commands/Base/CommandBase.cs
@@ -0,0 +1,29 @@
+﻿using MvvmHelpers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AniSpace.Infructuctre.Commands.Base
+{
+    internal abstract class CommandBase : IAsyncCommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        protected void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public abstract bool CanExecute(object? parameter);
+
+        public abstract void Execute(object? parameter);
+
+        public abstract Task ExecuteAsync();
+    }
+}
---
+++ AniSpace/Infructuctre/Commands/Base/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniSpace.Infructuctre.Commands.Base
+{
+    internal class RelayCommand : CommandBase
+    {
+        private readonly Func<Task> _AsyncExecute;
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExcute;
+
+        internal RelayCommand(Func<Task> AsyncExecute, Action<object> Execute, Func<object, bool> CanExcute)
+        {
+            _AsyncExecute = AsyncExecute;
+            _Execute = Execute;
+            _CanExcute = CanExcute;
+
+        }
+        public override bool CanExecute(object? parameter) => _CanExcute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
+
+        public override Task ExecuteAsync() => _AsyncExecute();
+    }
+}
